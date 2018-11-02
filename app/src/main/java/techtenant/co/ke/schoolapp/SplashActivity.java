ed71a52f6b45454396aafe55351f03a3495989f6
techtenant <bnyilima@gmail.com>
2018-11-02 04:30:06 +0300

    first commit

---
+++ app/src/main/java/techtenant/co/ke/schoolapp/SplashActivity.java
@@ -0,0 +1,222 @@
+package techtenant.co.ke.schoolapp;
+
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.rengwuxian.materialedittext.MaterialEditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import dmax.dialog.SpotsDialog;
+import techtenant.co.ke.schoolapp.model.User;
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
+
+public class SplashActivity extends AppCompatActivity {
+    @BindView(R.id.btnsignin)
+    Button btnSignIn;
+    @BindView(R.id.btnregister) Button btnReg;
+    FirebaseAuth auth;
+    FirebaseDatabase db;
+    DatabaseReference users;
+    RelativeLayout rootLayout;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+        .setDefaultFontPath("fonts/arkhip_font.ttf")
+        .setFontAttrId(R.attr.fontPath)
+        .build());
+        setContentView(R.layout.activity_splash);
+        ButterKnife.bind(this);
+         auth = FirebaseAuth.getInstance();
+         db = FirebaseDatabase.getInstance();
+         users = db.getReference("Users");
+        rootLayout = (RelativeLayout) findViewById(R.id.rootLayout);
+
+        btnReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showRegisterDialog();
+            }
+        });
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showLoginDialog();
+
+            }
+        });
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
+
+    private void showLoginDialog() {
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Login");
+        dialog.setMessage("Please Use Email to Login");
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View layout_login = inflater.inflate(R.layout.layout_login, null);
+        final MaterialEditText edtMail = layout_login.findViewById(R.id.edtEmail);
+        final MaterialEditText edtPass = layout_login.findViewById(R.id.edtPass);
+
+
+        dialog.setView(layout_login);
+        dialog.setPositiveButton("LOGIN", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+                if (TextUtils.isEmpty(edtMail.getText().toString())) {
+                    Snackbar.make(rootLayout, "Please Enter Email Address", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(edtPass.getText().toString())) {
+                    Snackbar.make(rootLayout, "Please Enter Your Password", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (edtPass.getText().toString().length() < 6) {
+                    Snackbar.make(rootLayout, "Password too short !!! Make more than 6 characters", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                auth.signInWithEmailAndPassword(edtMail.getText().toString(),
+                        edtPass.getText().toString())
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                startActivity(new Intent(SplashActivity.this, WelcomeActivity.class));
+                                finish();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Snackbar.make(rootLayout, "Failed "+e.getMessage(), Snackbar.LENGTH_SHORT)
+                                .show();
+                    }
+                });
+            }
+
+        });
+
+        dialog.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        dialog.show();
+    }
+
+
+    private void  showRegisterDialog() {
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Register");
+        dialog.setMessage("Please User Email to Register");
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View reg_layout = inflater.inflate(R.layout.layout_register, null);
+        final MaterialEditText edtMail = reg_layout.findViewById(R.id.edtEmail);
+        final MaterialEditText edtName = reg_layout.findViewById(R.id.edtName);
+        final MaterialEditText edtPass= reg_layout.findViewById(R.id.edtPass);
+        final MaterialEditText edtPhone = reg_layout.findViewById(R.id.edtPhone);
+
+        dialog.setView(reg_layout);
+        dialog.setPositiveButton("REGISTER", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+                if (TextUtils.isEmpty(edtMail.getText().toString())) {
+                    Snackbar.make(rootLayout, "Please Enter Email Address", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(edtName.getText().toString())) {
+                    Snackbar.make(rootLayout, "Please Enter Your Name", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(edtPass.getText().toString())) {
+                    Snackbar.make(rootLayout, "Please Enter Your Password", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(edtPhone.getText().toString())) {
+                    Snackbar.make(rootLayout, "Please Your Phone Number", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if (edtPass.getText().toString().length()<6){
+                    Snackbar.make(rootLayout, "Password too short !!! Make more than 6 characters", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                //AlertDialog waitingDialog = new SpotsDialog.Builder().setContext(SplashActivity.this);
+                //waitingDialog.show();
+                auth.createUserWithEmailAndPassword(edtMail.getText().toString(),
+                        edtPass.getText().toString())
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                User user = new User();
+                                user.setEmail(edtMail.getText().toString());
+                                user.setName(edtName.getText().toString());
+                                user.setPassword(edtPass.getText().toString());
+                                user.setPhone(edtPhone.getText().toString());
+
+                                users.child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                        .setValue(user)
+                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+                                                Snackbar.make(rootLayout, "User Registered Successfully", Snackbar.LENGTH_SHORT)
+                                                        .show();
+                                            }
+                                        })
+                                        .addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                Snackbar.make(rootLayout, "Failed "+e.getMessage(), Snackbar.LENGTH_SHORT)
+                                                        .show();
+                                            }
+                                        });
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Snackbar.make(rootLayout, "Failed "+e.getMessage(), Snackbar.LENGTH_SHORT)
+                                        .show();
+                            }
+                        });
+            }
+        });
+        dialog.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        dialog.show();
+
+    }
+}
---
+++ app/src/main/java/techtenant/co/ke/schoolapp/WelcomeActivity.java
@@ -0,0 +1,68 @@
+package techtenant.co.ke.schoolapp;
+
+import android.graphics.Color;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.firebase.geofire.GeoFire;
+import com.firebase.geofire.GeoLocation;
+import com.firebase.geofire.GeoQuery;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
+import java.util.Map;
+
+public class WelcomeActivity extends FragmentActivity implements OnMapReadyCallback {
+    private static final GeoLocation INITIAL_CENTER = new GeoLocation(-1.326755, 36.852706);
+    private static final int INITIAL_ZOOM_LEVEL = 14;
+    private static final String GEO_FIRE_DB = "https://publicdata-transit.firebaseio.com";
+    private static final String GEO_FIRE_REF = GEO_FIRE_DB + "/_geofire";
+
+    private GoogleMap map;
+    private Circle searchCircle;
+    private GeoFire geoFire;
+    private GeoQuery geoQuery;
+
+    private Map<String,Marker> markers;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        this.map = mapFragment.getMap();
+        LatLng latLngCenter = new LatLng(INITIAL_CENTER.latitude, INITIAL_CENTER.longitude);
+        this.searchCircle = this.map.addCircle(new CircleOptions().center(latLngCenter).radius(1000));
+        this.searchCircle.setFillColor(Color.argb(66, 255, 0, 255));
+        this.searchCircle.setStrokeColor(Color.argb(66, 0, 0, 0));
+        this.map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLngCenter, INITIAL_ZOOM_LEVEL));
+        this.map.setOnCameraChangeListener(this);
+
+        FirebaseOptions options = new FirebaseOptions.Builder().setApplicationId("geofire").setDatabaseUrl(GEO_FIRE_DB).build();
+        FirebaseApp app = FirebaseApp.initializeApp(this, options);
+    }
+
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        map.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
